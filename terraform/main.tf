578179053ac287212b4be1f936d8beb37ea471ec
thompsonanigbo <103883140+thompsonanigbo@users.noreply.github.com>
2025-12-30 10:49:14 +0000

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -23,12 +23,12 @@ module "vpc" {
 
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  kubernetes_version = "1.33"
+  version = "~> 21.0"
 
-  name                   = local.name
-  endpoint_public_access = true
+  cluster_name                   = local.name
+  cluster_endpoint_public_access = true
 
-  addons = {
+  cluster_addons = {
     coredns = {
       most_recent = true
     }
@@ -45,12 +45,12 @@ module "eks" {
   control_plane_subnet_ids = module.vpc.intra_subnets
 
   # EKS Managed Node Group(s)
-  #eks_managed_node_groups = {
-  #  ami_type       = "AL2023_x86_64_STANDARD"
-  #  instance_types = ["m5.xlarge"]
+  eks_managed_node_group_defaults = {
+    ami_type       = "AL2_x86_64"
+    instance_types = ["m5.large"]
 
-  #  attach_cluster_primary_security_group = true
-  #}
+    attach_cluster_primary_security_group = true
+  }
 
   eks_managed_node_groups = {
     utom-cluster-wg = {
@@ -59,7 +59,7 @@ module "eks" {
       desired_size = 1
 
       instance_types = ["t3.large"]
-      #capacity_type  = "SPOT"
+      capacity_type  = "SPOT"
 
       tags = {
         ExtraTag = "helloworld"
